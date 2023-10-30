2b04faec63306dee6f1adb95fa51e70fa67f549b
FabioVasi <vasifabio96@gmail.com>
2023-10-30 18:12:39 +0100

    add cicle

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Train;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,21 @@ class TrainsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 12; $i++) {
+            $train = new Train();
+            $train->company =  $faker->word();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->dateTimeBetween('now', '+10 days');
+            $train->arrival_time = $faker->dateTimeBetween('now', '+10 days');
+            $train->train_code = $faker->bothify('???-###');
+            $train->carriage_number = $faker->numberBetween(1, 13);
+            $train->in_time = $faker->boolean();
+            $train->deleted = $faker->boolean();
+
+            $train->save();
+        }
     }
 }
